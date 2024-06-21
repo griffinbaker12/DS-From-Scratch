6c15e6c7b916f168c20897012cad57ff0739d303
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-06-21 13:14:28 -0500

    ds scratch

--- ch4_linear_alg.py
+++ ch4_linear_alg.py
@@ -1,4 +1,5 @@
-from typing import List
+import math
+from typing import Callable, List, Tuple
 
 Vector = List[float]
 
@@ -86,3 +87,83 @@ def dot_product(v: Vector, w: Vector) -> Vector:
 
 
 assert dot_product([1, 2, 3], [4, 5, 6]) == 32
+
+
+# just dot product with itself
+def sum_of_squares(v: Vector) -> Vector:
+    return dot_product(v, v)
+
+
+def magnitude(v: Vector) -> Vector:
+    return math.sqrt(sum_of_squares(v))
+
+
+assert magnitude([3, 4]) == 5
+
+
+def squared_distance(v: Vector, w: Vector) -> float:
+    return sum_of_squares(subtract(v, w))
+
+
+# def distance(v: Vector, w: Vector) -> float:
+#     return math.sqrt(squared_distance(v, w))
+
+
+def distance(v: Vector, w: Vector) -> float:
+    return magnitude(subtract(v, w))
+
+
+Matrix = List[List[float]]
+
+A = [
+    [1, 2, 3],
+    [4, 5, 6],
+]
+
+B = [[1, 2], [3, 4], [5, 6]]
+
+
+def shape(A: Matrix) -> Tuple[int, int]:
+    return len(A), len(A[0]) if len(A[0]) else 0
+
+
+s = shape([[1, 2, 3], [4, 5, 6]])
+
+assert shape([[1, 2, 3], [4, 5, 6]]) == (2, 3)
+
+
+def get_row(A: Matrix, i: int) -> Vector:
+    return A[i]
+
+
+def get_column(A: Matrix, j: int) -> Vector:
+    return [A_i[j] for A_i in A]  # jth element in row A_i
+
+
+def make_matrix(
+    num_rows: int,
+    num_cols: int,
+    entry_fn: Callable[[int, int], float],
+) -> Matrix:
+    return [[entry_fn(i, j) for j in range(num_cols)] for i in range(num_rows)]
+
+
+def make_matrix_no_comp(
+    num_rows: int,
+    num_cols: int,
+    entry_fn: Callable[[int, int], float],
+) -> Matrix:
+    matrix = []
+    for i in range(num_rows):
+        row = []
+        for j in range(num_cols):
+            row.append(entry_fn(i, j))
+        matrix.append(row)
+    return matrix
+
+
+def identity_matrix(n: int) -> Matrix:
+    """
+    Returns n x n identity matrix
+    """
+    return make_matrix(n, n, lambda i, j: 1 if i == j else 0)
