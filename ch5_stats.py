35005a4a211f1e6f70a5e936fba84f21bf8128a0
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-06-23 17:11:58 -0500

    ch5

--- ch5_stats.py
+++ ch5_stats.py
@@ -4,7 +4,13 @@ from typing import List
 
 import matplotlib.pyplot as plt
 
-from ch4_linear_alg import dot, sum_of_squares
+from ch4_linear_alg import dot_product, sum_of_squares
+
+x = [
+    1,
+    2,
+    3,
+]
 
 num_friends = [
     100.0,
@@ -544,4 +550,37 @@ daily_minutes = [
 def covariance(xs: List[float], ys: List[float]) -> float:
     assert len(xs) == len(ys), "xs and ys must have the same # of elements"
 
-    return dot(de_mean(xs), de_mean(ys)) / (len(xs) - 1)
+    return dot_product(de_mean(xs), de_mean(ys)) / (len(xs) - 1)
+
+
+daily_hours = [dm / 60 for dm in daily_minutes]
+
+assert 22.42 < covariance(num_friends, daily_minutes) < 22.43
+assert 22.42 / 60 < covariance(num_friends, daily_hours) < 22.43 / 60
+
+outlier = num_friends.index(100)  # index of outlier
+
+
+def correlation(xs: List[float], ys: List[float]) -> float:
+    """Measures how much xs and ys vary in tandem about their means"""
+    stdev_x = standard_deviation(xs)
+    stdev_y = standard_deviation(ys)
+    if stdev_x > 0 and stdev_y > 0:
+        return covariance(xs, ys) / stdev_x / stdev_y
+    else:
+        return 0  # if no variation, correlation is zero
+
+
+assert 0.24 < correlation(num_friends, daily_minutes) < 0.25
+assert 0.24 < correlation(num_friends, daily_hours) < 0.25
+
+outlier = num_friends.index(100)  # index of outlier
+
+num_friends_good = [x for i, x in enumerate(num_friends) if i != outlier]
+
+daily_minutes_good = [x for i, x in enumerate(daily_minutes) if i != outlier]
+
+daily_hours_good = [dm / 60 for dm in daily_minutes_good]
+
+assert 0.57 < correlation(num_friends_good, daily_minutes_good) < 0.58
+assert 0.57 < correlation(num_friends_good, daily_hours_good) < 0.58
